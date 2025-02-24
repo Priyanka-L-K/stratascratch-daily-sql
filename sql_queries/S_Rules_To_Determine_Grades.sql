0c4d4598636f3bf38c6792269125725ee0f0e221
Priyanka Lakur Krishnamurthy <67971841+Priyanka-L-K@users.noreply.github.com>
2025-02-23 18:09:15 -0600

    Create S_Rules_To_Determine_Grades.sql

---
+++ sql_queries/S_Rules_To_Determine_Grades.sql
@@ -0,0 +1,8 @@
+select grade, max(score) as upper_bound, min(score) as lower_bound, 
+case 
+    when grade = 'A' then concat('Score > ', max(score) ,' AND Score <= ', min(score), ' => Grade = A')
+    when grade = 'B' then concat('Score > ', max(score) ,' AND Score <= ', min(score), ' => Grade = B')
+    when grade = 'C' then concat('Score > ', max(score) ,' AND Score <= ', min(score), ' => Grade = C')
+end as rule
+from los_angeles_restaurant_health_inspections
+group by grade;
