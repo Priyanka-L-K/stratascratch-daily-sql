313472efdd159acfa264139db82efe66e9869585
Priyanka Lakur Krishnamurthy <67971841+Priyanka-L-K@users.noreply.github.com>
2025-02-25 23:44:16 -0600

    Delete Avg_Earnings_per_Weekday_and_Hour.sql

--- Avg_Earnings_per_Weekday_and_Hour.sql
+++
@@ -1,6 +0,0 @@
-My answer
-
-select DAYNAME(customer_placed_order_datetime) as day_of_week, HOUR(customer_placed_order_datetime) as hour_day, round(avg(order_total - discount_amount + tip_amount - refunded_amount), 2) as net_order_total
-from doordash_delivery
-group by day_of_week, hour_day
-order by field(day_of_week, 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'), hour_day;
