43e766b4764325ba0a5d464b1af55b90ae9332c2
Priyanka Lakur Krishnamurthy <67971841+Priyanka-L-K@users.noreply.github.com>
2025-02-28 18:17:08 -0600

    Update Find_year_in_which_shortest_athlete_participated.sql

--- sql_queries/Find_year_in_which_shortest_athlete_participated.sql
+++ sql_queries/Find_year_in_which_shortest_athlete_participated.sql
@@ -1,5 +1,20 @@
+-- need to consider same height in multiple years
+
+-- My answe:
+
 select year
 from olympics_athletes_events
 where height IS NOT NULL
 order by height asc
 limit 1;
+
+-- Optimized:
+
+WITH shortest_height AS (
+    SELECT MIN(height) AS min_height
+    FROM olympics_athletes_events
+    WHERE height IS NOT NULL
+)
+SELECT year, height
+FROM olympics_athletes_events
+WHERE height = (SELECT min_height FROM shortest_height);
