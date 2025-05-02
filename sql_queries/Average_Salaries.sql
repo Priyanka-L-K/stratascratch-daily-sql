7d2ec770293927c99daa500167d6461587b7bdba
Priyanka Lakur Krishnamurthy <67971841+Priyanka-L-K@users.noreply.github.com>
2025-05-02 13:10:17 -0500

    Update Average_Salaries.sql

--- sql_queries/Average_Salaries.sql
+++ sql_queries/Average_Salaries.sql
@@ -1,3 +1,4 @@
+Mine:
 with avg_s as 
 (select department, avg(salary) as avg_salary
 from employee
@@ -7,3 +8,13 @@ select e.department, e.first_name, e.salary, a.avg_salary
 from employee as e
 inner join avg_s as a
 on e.department = a.department;
+
+
+Optimized:
+SELECT 
+  department,
+  first_name,
+  salary,
+  AVG(salary) OVER (PARTITION BY department) AS avg_salary
+FROM employee
+ORDER BY department;
